592604b770c2e2514e662b72bf9849bccd6f248c
jonwang2000 <37186169+jonwang2000@users.noreply.github.com>
2019-07-21 20:16:59 -0400

    Simplistic timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Timer from "./components/Timer";
 import MainButton from "./components/MainButton";
 
 function App() {
+	const [timer, setTimer] = useState(0);
+	const [timerOn, setTimerOn] = useState(false);
+
+	useEffect(() => {
+		let interval = null;
+		if (timerOn) {
+			interval = setInterval(() => {
+				setTimer(timer + 1);
+			}, 1000);
+		} else if (!timerOn && timer !== 0) {
+			clearInterval(interval);
+		}
+        return () => clearInterval(interval);
+	}, [timerOn, timer]);
+
 	return (
 		<div>
-			<Timer />
-			<MainButton />
+            <p>{timer}</p>
+
+			<Timer timer={timer} setTimer={setTimer} />
+			<MainButton timerOn={timerOn} setTimerOn={setTimerOn} />
+
+            <button onClick={() => setTimerOn(!timerOn)}>Button!</button>
 		</div>
 	);
 }
